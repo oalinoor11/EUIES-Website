c8936b8cf0b4f4c62d4f62d9fbd7883e3a655edc
oalinoor11 <oalinoor11@gmail.com>
2021-08-22 22:57:09 +0600

    slide image added

--- lib/Presentation/Pages/homescreen.dart
+++ lib/Presentation/Pages/homescreen.dart
@@ -7,8 +7,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  PageController pageController = PageController();
-  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(backgroundColor: Colors.white,
