a8850f22ee352746b3c3ca6917540f6736dbee93
oalinoor11 <oalinoor11@gmail.com>
2021-08-23 13:33:14 +0600

    container text corrected

--- lib/Presentation/Pages/homescreen.dart
+++ lib/Presentation/Pages/homescreen.dart
@@ -41,29 +41,24 @@ class _HomeScreenState extends State<HomeScreen> {
               SingleChildScrollView(scrollDirection: Axis.horizontal,
                 child: Column(
                   children: [Container(),
-                    Container(decoration: BoxDecoration(color: Colors.green, borderRadius: BorderRadius.circular(5)),
-                      height: 500,
+                    Container(decoration: BoxDecoration(color: Colors.black12, borderRadius: BorderRadius.circular(10)),
+                      height: 550,
                       width: 400,
                         child: Column(crossAxisAlignment: CrossAxisAlignment.start,
                             children: [Container(),
                           const Image(
                             image: AssetImage("assets/images/flag.png"),
-                            width: double.infinity,
                           ),
                               SizedBox(height: 15,),
-                              Row(
-                                children: [
-                                  SizedBox(width: 15,),
-                                  Text('Study Abroad', style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),),
-                                ],
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text('Study Abroad', style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),),
                               ),
-                              SizedBox(height: 15,),
-                              Row(
-                                children: [
-                                  SizedBox(width: 15,),
-                                  Text('We provide expert services for students to fulfill their dreams of obtaining foreign education. Our students are placed all around the globe and we are proud to have helped students get admitted into top universities in destinations such as USA, Europe, Australia and more.',
-                                    style: TextStyle(color: Colors.black, fontSize: 20,),textAlign: TextAlign.start,),
-                                ],
+                              SizedBox(height: 80,),
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text('We provide expert services for students to fulfill their dreams of obtaining foreign education. Our students are placed all around the globe and we are proud to have helped students get admitted into top universities in destinations such as USA, Europe, Australia and more.',
+                                  style: TextStyle(color: Colors.black, fontSize: 20,), softWrap: true,),
                               ),
                         ],
                         ),
